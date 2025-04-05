b65c2bd6cef0cc829b7d6680ead2fd1c88f6e151
Muhammad Ravat <m0ravat736@gmail.com>
2025-04-05 22:22:56 +0100

    Tree remove times: w/o search - 77281098500 with search: 79467648600

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,33 @@
 package org.example;
 
+import org.dataStructure.tree.BinarySearchTree;
+import org.dataStructure.tree.TreeNode;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        time();
     }
 
-    public static String time() {
-        long start = System.nanoTime();
-
-        // -------- program --------
-
-        // -------------------------------
-
-        long duration = (System.nanoTime() - start) / 1000000;
-        return duration + "ms";
+    public static void time() {
+        long total = 0;
+        for (int i = 0; i < 1000000000; i++) {
+            long start = System.nanoTime();
+            BinarySearchTree binaryTree = new BinarySearchTree();
+            binaryTree.insert(new TreeNode(5));
+            binaryTree.insert(new TreeNode(1));
+            binaryTree.insert(new TreeNode(9));
+            binaryTree.insert(new TreeNode(2));
+            binaryTree.insert(new TreeNode(7));
+            binaryTree.insert(new TreeNode(3));
+            binaryTree.insert(new TreeNode(6));
+            binaryTree.insert(new TreeNode(4));
+            binaryTree.insert(new TreeNode(8));
+            binaryTree.remove(3);
+            long duration = (System.nanoTime() - start);
+            total += duration;
+        }
+        System.out.println("Total time: " + total);
     }
 }
